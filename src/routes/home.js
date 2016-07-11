ad01fa70478e015ae245fb8de5d738e1f644fcc7
Dan Zhang <danzhang@Dans-MacBook-Pro-2.local>
2016-07-11 11:14:49 -0700

    Use 500px api to get popular photo data on page load

--- src/routes/home.js
+++ src/routes/home.js
@@ -3,30 +3,24 @@ import React from 'react';
 class Home extends React.Component {
 
   state = {
-    data : {}
+    data : [],
+    page: 1
   }
 
   componentDidMount() {
-    // fetch("/v1/proxy", {
-    //   method: 'POST',
-    //   headers: {
-    //     'Accept': 'application/json',
-    //     'Content-Type': 'application/json'
-    //   },
-    //   body: JSON.stringify({
-    //     method: 'GET',
-    //     url: 'gin1.com/healthcheck',
-    //     headers: {
-    //       'content-type' : 'json'
-    //     }
-    //   })
-    // }).then((response) => {
-    //   return response;
-    // }).then((data) => {
-    //   this.setState({
-    //     data: data.json()
-    //   })
-    // })
+    this.getData(this.state.page);
+  }
+
+  getData(page) {
+    fetch(`https://api.500px.com/v1/photos?feature=popular&page=${page}&consumer_key=g34gnWJqKAIyYcrgOIFOjrGJB5XTm2Z9sxoElJbQ`).then((response) => {
+      return response.json();
+    }).then((data) => {
+      console.log(data);
+      this.setState({
+        page: this.state.page + 1,
+        data: this.state.data.concat(data.photos)
+      })
+    })
   }
 
   render() {
